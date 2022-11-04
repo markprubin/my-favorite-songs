fa81e124a0bf8c774d13a3046331018128a0a816
markprubin <mark.p.rubin@gmail.com>
2022-11-04 11:50:54 -0700

    adds create action, route, and request

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,25 @@
 class SongsController < ApplicationController
+
+  def index
+    songs = Song.all
+    render json: songs.as_json
+  end
+
+  def show
+    songs = Song.find_by(id: params["id"])
+    render json: songs.as_json
+  end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year],
+    )
+    render json: song.as_json
+    song.save
+  end
+
+
 end
