a73003f1cbcbb53c5d26a83991f31012c3e16c2a
markprubin <mark.p.rubin@gmail.com>
2022-11-04 11:08:13 -0700

    adds controller, updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Song.create(title: "Mr. Brightside", album: "Hot Fuss", artist: "The Killers", year: 2004)
+
+Song.create(title: "Lions in the Wild", album: "Lions in the Wild", artist: "Martin Garrix", year: 2016)
+
+Song.create(title: "Need You", album: "Need You", artist: "Dillon Francis", year: 2016)
+
+Song.create(title: "On My Mind", album: "Diplo", artist: "Diplo", year: 2022)
+
+Song.create(title: "Without You", album: "AVICI(01)", artist: "Avicii", year: 2017)
