6899a85d91877c804c8eb94f4957748740174db3
markprubin <mark.p.rubin@gmail.com>
2022-11-04 12:05:47 -0700

    adds update action, route, and request and destroy action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,5 +21,21 @@ class SongsController < ApplicationController
     song.save
   end
 
+  def update
+    song = Song.find_by(id: params["id"])
+    
+      song.title = params[:title] || song.title,
+      song.album = params[:album] || song.album,
+      song.artist = params[:artist] || song.artist,
+      song.year = params[:year] || song.year,
+      song.save
+    render json: song.as_json
+  end
 
+  def destroy
+    song = Song.find_by(id: params["id"])
+    song.destroy
+
+    render json: (message: "Song Deleted")
+  end
 end
